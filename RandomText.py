3a85b8381d554853945d883d186a5102de32adf4
Pawnas-Ubuntu <deividas.mas1@gmail.com>
2018-05-26 23:48:46 +0100

    Adding the function for generating random text to a file

---
+++ RandomText.py
@@ -0,0 +1,10 @@
+def randomText():
+	text_file = open('randomtext.txt', 'w')
+	import numpy, random
+	target_length = 456565
+	random_string = ""
+	while len(random_string) < target_length:
+		random_string += random.choice('abcdefg ')
+		text_file.write(random_string)
+	random_text = random_string.split()
+	text_file.close()
